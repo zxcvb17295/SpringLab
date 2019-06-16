589fefb70aec6861dd4b87fbe0825319982337cb
G-mi <zxcvb17295@gmail.com>
2019-06-16 12:33:43 +0800

    新增範例 spring-boot-h2

---
+++ spring-boot-h2/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user (id, username, name, age, balance) values (1, 'account1', 'name1', 20, 100.00);
+insert into user (id, username, name, age, balance) values (2, 'account2', 'name2', 28, 180.00);
+insert into user (id, username, name, age, balance) values (3, 'account3', 'name3', 32, 280.00);
